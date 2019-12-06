d206037e9679a463902f693e943818437d5bd87f
alfonsserra <alfonsoserra@gmail.com>
2019-12-06 08:36:16 +0100

    Add a decorator for test elements

--- src/widgets/systelab-dialog-test.ts
+++ src/widgets/systelab-dialog-test.ts
@@ -1,6 +1,41 @@
 import { by } from 'protractor';
 import { Widget } from './widget-test';
 import { Button } from './button-test';
+import { FormInputElement } from '../services';
+import { InputableInterface } from './inputable.interface';
+import 'reflect-metadata';
+
+export interface IAttributeProperties {
+	name?: string;
+	value?: string;
+	type?: AttributeType;
+	isVisible?: boolean;
+}
+
+export enum AttributeType {
+	Text,
+	Date,
+	Number,
+	Password
+}
+
+export const ATTRIBUTE_PREFIX = 'test:';
+
+/**
+ * Adds attribute metadata to a property
+ * @param {IAttributeProperties} attributes
+ * @returns {(target: any, propertyKey: string) => void}
+ * @constructor
+ */
+export function TestAttribute(attributes: IAttributeProperties) {
+	return (target: object, propertyKey: string) => {
+		if (attributes !== undefined && attributes !== null) {
+			Object.keys(attributes).forEach(key => {
+				Reflect.defineMetadata(`${ATTRIBUTE_PREFIX}${key}`, attributes[key], target, propertyKey);
+			});
+		}
+	};
+}
 
 export class SystelabDialogTest extends Widget {
 
@@ -28,5 +63,22 @@ export class SystelabDialogTest extends Widget {
 	public getButtonByName(name: string): Button {
 		return new Button(this.elem
 			.element(by.buttonText(name)));
+	}
+
+	public getInputElements(i?: number): FormInputElement[] {
+		const form: FormInputElement[] = [];
+		for (var member in this) {
+			if (!this.hasOwnProperty(member)) {
+				let name = Reflect.getMetadata('test:name', this, member);
+				let type = Reflect.getMetadata('test:type', this, member);
+				let value = Reflect.getMetadata('test:value', this, member);
+				if (name) {
+					let method: any = this[member];
+					let widget = Reflect.apply(method, this, []);
+					form.push({field: <InputableInterface>widget, name: name, value: value})
+				}
+			}
+		}
+		return form;
 	}
 }
