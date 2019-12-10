bc09e2a513b6e9dff0503a58b076a5c7b6135198
alfonsserra <alfonsoserra@gmail.com>
2019-12-10 22:30:58 +0100

    Implement initial service

--- src/services/test-attributes.service.ts
+++ src/services/test-attributes.service.ts
@@ -1,28 +1,74 @@
 import { Button } from '../widgets/button-test';
 import { Widget } from '../widgets/widget-test';
+import { InputField } from '../widgets/inputfield-test';
 
 export class TestAttributesService {
 
-	public static isButton(toBeDetermined: Widget): boolean {
-		return toBeDetermined instanceof Button;
-	}
 
-	public static check(dialog) {
-		for (var member in dialog) {
-			console.log(member);
-			if (!dialog.hasOwnProperty(member)) {
-				let name = Reflect.getMetadata('test:name', dialog, member);
-				let type = Reflect.getMetadata('test:type', dialog, member);
-				let enable = Reflect.getMetadata('test:enable', dialog, member);
-				let length = Reflect.getMetadata('test:length', dialog, member);
+	public static async check(dialog) {
+		const m: Array<string> = Object.getOwnPropertyNames(Object.getPrototypeOf(dialog));
+
+		for (let i = 0; i < m.length; i++) {
+				let name = Reflect.getMetadata('test:name', dialog, m[i]);
+				let type = Reflect.getMetadata('test:type', dialog, m[i]);
+				let enable = Reflect.getMetadata('test:enable', dialog, m[i]);
+				let visible = Reflect.getMetadata('test:visible', dialog, m[i]);
+				let label = Reflect.getMetadata('test:label', dialog, m[i]);
+				let mandatory = Reflect.getMetadata('test:mandatory', dialog, m[i]);
+				let length = Reflect.getMetadata('test:length', dialog, m[i]);
 				if (name) {
-					let method: any = dialog[member];
-					let widget = Reflect.apply(method, dialog, []);
+					let method: any = dialog[m[i]];
+					let widget = await Reflect.apply(method, dialog, []);
 					if (this.isButton(widget)) {
-						expect((<Button>widget).isEnabled()).toEqual(enable);
+						await this.checkButton(<Button>widget, enable, visible, label, mandatory, length)
+					} else if (this.isInputField(widget)) {
+						await this.checkInputField(<InputField>widget, enable, visible, label, mandatory, length)
 					}
 				}
-			}
 		}
+	}
+
+	private static async checkButton(button: Button, enable: boolean, visible: boolean, label: boolean, mandatory: boolean, length: number) {
+		if (enable!==undefined) {
+			await expect(button.isEnabled()).toEqual(enable);
+		}
+	}
+
+	private static async checkInputField(input: InputField, enable: boolean, visible: boolean, label: boolean, mandatory: boolean, length: number) {
+		if (enable!==undefined) {
+			await expect(input.isEnabled()).toEqual(enable);
+		}
+		if (visible!==undefined) {
+			await expect(input.isDisplayed).toEqual(visible);
+		}
+		if (length!==undefined) {
+			let text=this.random(length);
+			await input.clear();
+			await input.setText(text);
+			await expect(input.getText()).toEqual(text);
+
+			let text2=this.random(length+1);
+			await input.clear();
+			await input.setText(text2);
+			await expect(input.getText()).toEqual(text2.substring(0,length));
+		}
+	}
+
+	public static isButton(widget: Widget): boolean {
+		return widget instanceof Button;
+	}
+
+	public static isInputField(widget: Widget): boolean {
+		return widget instanceof InputField;
+	}
+
+	private static random(length) {
+		var result           = '';
+		var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		var charactersLength = characters.length;
+		for ( let i = 0; i < length; i++ ) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
 	}
 }
