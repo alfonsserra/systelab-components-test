f1eabd18b5da3a305c2f3a02575c9d4af67d7dc0
alfonsserra <alfonsoserra@gmail.com>
2020-01-07 09:53:47 +0100

    Accessor to input

--- src/widgets/datepicker-test.ts
+++ src/widgets/datepicker-test.ts
@@ -1,22 +1,26 @@
-import { by, protractor } from "protractor";
+import { by, ElementFinder, protractor } from "protractor";
 import { Widget } from './widget-test';
 
 export class Datepicker extends Widget
 {
     public async isPresent(): Promise<boolean>
     {
-        return await this.elem.element(by.css("input")).isPresent();
+        return await this.getInputElement().isPresent();
     }
 
     public async getValue(): Promise<string>
     {
-        return await this.elem.element(by.css("input")).getAttribute("value");
+        return await this.getInputElement().getAttribute("value");
     }
 
     public async setValue(value: string): Promise<void>
     {
-        await this.elem.element(by.css("input")).clear();
-        await this.elem.element(by.css("input")).sendKeys(value);
-        await this.elem.element(by.css("input")).sendKeys(protractor.Key.TAB);
+        await this.getInputElement().clear();
+        await this.getInputElement().sendKeys(value);
+        await this.getInputElement().sendKeys(protractor.Key.TAB);
+    }
+
+    private getInputElement(): ElementFinder {
+        return this.elem.element(by.css("input"));
     }
 }
