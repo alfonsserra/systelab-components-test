864e1de72840a1910ab4d94c70740ef144aab160
fperez1 <fperez1@werfen.com>
2020-02-03 15:37:12 +0100

    method to click a label is added

--- src/widgets/label.ts
+++ src/widgets/label.ts
@@ -5,4 +5,8 @@ export class Label extends Widget {
 	public async getText(): Promise<string> {
 		return await this.elem.getText()
 	}
+
+	public async click(): Promise<void> {
+		await this.elem.click();
+	}
 }
